c9cacaa5fb85dd65bd21e95355effce8c21ab9ab
lruler <1204312199@qq.com>
2021-12-07 17:21:08 +0800

    http / express

--- base/mysql/base.js
+++ base/mysql/base.js
@@ -1,13 +1,17 @@
 var mysql = require('mysql'); //引入模块
 
 var connection = mysql.createConnection({ //设置参数
-  host     : 'localhost',
-  user     : 'me',
-  password : 'secret',
-  database : 'my_db'
+  host: 'localhost',
+  port: '3306', // 可选 默认3306
+  user     : 'root',
+  password : '954595',
+  database : 'test'
 });
  
-connection.connect(); //连接数据库
+connection.connect((err) => {
+  if (err) console.log(err)
+  else console.log('数据库连接成功')
+}); //连接数据库
 
 
 //执行数据库操作并执行回调，有err则抛出异常，控制台输出结果 
